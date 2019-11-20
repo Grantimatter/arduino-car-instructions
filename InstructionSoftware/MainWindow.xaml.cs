2d64bd1901049857c1b7f6224c0c5f4870c6380d
Grant Wiswell <GT101boy@gmail.com>
2019-11-20 14:06:11 -0600

    Creating custom ui element

--- InstructionSoftware/MainWindow.xaml.cs
+++ InstructionSoftware/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using InstructionSoftware.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,6 +26,75 @@ namespace InstructionSoftware
         {
             InitializeComponent();
             DataContext = new MainWindowViewModel();
+        }
+
+        public static readonly DependencyProperty ScaleValueProperty = DependencyProperty.Register("ScaleValue", typeof(double), typeof(MainWindow), new UIPropertyMetadata(1.0, new PropertyChangedCallback(OnScaleValueChanged), new CoerceValueCallback(OnCoerceScaleValue)));
+
+        private static object OnCoerceScaleValue(DependencyObject o, object value)
+        {
+            MainWindow mainWindow = o as MainWindow;
+            if (mainWindow != null)
+                return mainWindow.OnCoerceScaleValue((double)value);
+            else
+                return value;
+        }
+
+        private static void OnScaleValueChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            MainWindow mainWindow = o as MainWindow;
+            if (mainWindow != null)
+                mainWindow.OnScaleValueChanged((double)e.OldValue, (double)e.NewValue);
+        }
+
+        protected virtual double OnCoerceScaleValue(double value)
+        {
+            if (double.IsNaN(value))
+                return 1.0f;
+
+            value = Math.Max(0.1, value);
+            return value;
+        }
+
+        protected virtual void OnScaleValueChanged(double oldValue, double newValue)
+        {
+
+        }
+
+        public double ScaleValue
+        {
+            get
+            {
+                return (double)GetValue(ScaleValueProperty);
+            }
+            set
+            {
+                SetValue(ScaleValueProperty, value);
+            }
+        }
+
+        private void MainGrid_SizeChanged(object sender, EventArgs e)
+        {
+            CalculateScale();
+        }
+
+        private void CalculateScale()
+        {
+            double yScale = ActualHeight / 250f;
+            double xScale = ActualWidth / 200f;
+            double value = Math.Min(xScale, yScale);
+            ScaleValue = (double)OnCoerceScaleValue(main_window, value);
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            ProcessStartInfo ps = new ProcessStartInfo("https://github.com/Grantimatter/arduino-car-instructions/wiki")
+            {
+                UseShellExecute = true,
+                Verb = "open"
+            };
+
+            Process.Start(ps);
+            e.Handled = true;
         }
     }
 }
---
+++ InstructionSoftware/User Controls/InstructionBlock.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InstructionSoftware.User_Controls
+{
+    public partial class InstructionBlock : UserControl
+    {
+        public InstructionBlock()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public string InstructionLabel { get; set; }
+        public System.Windows.Media.SolidColorBrush bgColor { get; set; }
+        public int MaxLength { get; set; }
+    }
+}
