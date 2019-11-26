d29c84acd9b61f1ff48910e1def26d806c234ad8
Grant\GrantWiswell <GT101boy@gmail.com>
2019-11-26 13:29:41 -0600

    Changing Sliders

--- Arduino/ArduinoCarCodeSketch.ino
+++ Arduino/ArduinoCarCodeSketch.ino
@@ -22,8 +22,7 @@ void loop()
 		CarProgram car = CarProgram(PassPWM, PassFor, PassRev, PassMax, DrivPWM, DrivFor, DrivRev, DrivMax);
 		
 		// Fix the power variable
-		Instructions DOAFLIP = Instructions(2000, 10, 175, 100, 100, 10, 255, 255);
-		Instructions doATurn = Instructions(2000, 10, 175, 200, 100, 10, 75, 255);
+		Instructions SHUTDOWN = Instructions(0, 0, 0, 0, 0, 0, 0, 0);
 		Instructions ForwardGainMomentum = Instructions(2000, 0, 150, 200, 100, 10, 200, 200);
 		Instructions Forward = Instructions(0, 0, 100, 100, 0, 10, 100, 100);
 		Instructions Turn = Instructions(800, 10, 150, 150, 0, 10, -150, 150);
@@ -33,11 +32,6 @@ void loop()
 		car.RunInstruction(Forward);
 		car.RunInstruction(Turn);
 		car.RunInstruction(Reverse);
-		//car.RunInstruction(doATurn);
-
-		Instructions SHUTDOWN = Instructions(0, 0, 0, 0, 0, 0, 0, 0);
-
-		//car.RunInstruction(F1);
 		
 		car.RunInstruction(SHUTDOWN);
 
